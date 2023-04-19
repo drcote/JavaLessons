c0e75d4e02636da9c9b0c8a492219531bbd2b722
Denis Merezhin <merezhin@gmail.com>
2023-04-19 15:03:36 +0300

    Выполнено первое ДЗ.

---
+++ Lesson1/src/main/java/Arrays/Arrays.java
@@ -0,0 +1,119 @@
+package Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+        int[] arrayIO = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arrayIO.length; i++) {
+            arrayIO[i] = (arrayIO[i] == 1) ? 0 : 1;
+        }
+        System.out.println(java.util.Arrays.toString(arrayIO));
+
+        //2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+        int[] arrayMultip = new int[8];
+        for (int i = 0; i < arrayMultip.length; i++) {
+            arrayMultip[i] = 3 * i;
+        }
+        System.out.println(java.util.Arrays.toString(arrayMultip));
+
+        //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+        int[] arrayRnd = {7, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arrayRnd.length; i++) {
+            if (arrayRnd[i] < 6) {
+                arrayRnd[i] = arrayRnd[i] * 2;
+            }
+        }
+        System.out.println(java.util.Arrays.toString(arrayRnd));
+
+        //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
+        int lengthArray = 8;
+        int[][] twoDArray = new int[lengthArray][lengthArray];
+
+        for (int i = 0; i < lengthArray; i++) {
+            for (int j = 0; j < lengthArray; j++) {
+                // диагональ слева на право и справа на лево
+                twoDArray[i][j] = (i == j || j == lengthArray - 1 - i) ? 1 : 0;
+            }
+        }
+        print2DArray(twoDArray);
+
+        //5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
+        int indexMax = arrayRnd[0];
+        int indexMin = arrayRnd[0];
+        for (int i = 1; i < arrayRnd.length; i++) {
+            if (arrayRnd[i] > arrayRnd[indexMax]) {
+                indexMax = i;
+            }
+            if (arrayRnd[i] < arrayRnd[indexMin]) {
+                indexMin = i;
+            }
+        }
+        System.out.println("В массивае: " + java.util.Arrays.toString(arrayRnd));
+        System.out.println("Максимальное число: " + arrayRnd[indexMax]);
+        System.out.println("Минимальное число: " + arrayRnd[indexMin]);
+        //6. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны. Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true, checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами ||, эти символы в массив не входят.
+        System.out.println(checkArray(new int[]{1, 1, 1, 2, 1}));
+        //7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным), при этом метод должен сместить все элементы массива на n позиций. Элементы смещаются циклично. Для усложнения задачи нельзя пользоваться вспомогательными массивами. Примеры: [ 1, 2, 3 ] при n = 1 (на один вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1] при n = -2 (на два влево) -> [ 6, 1, 3, 5 ]. При каком n в какую сторону сдвиг можете выбирать сами.
+        System.out.println(java.util.Arrays.toString(shiftArray(new int[]{3, 5, 6, 1}, 3)));
+    }
+
+    private static void print2DArray(int[][] array) {
+        int lengthArray = array.length;
+        for (int i = 0; i < lengthArray; i++) {
+            for (int j = 0; j < lengthArray; j++) {
+                System.out.print(" " + array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean checkArray(int[] array) {
+        int indexRight = array.length - 1;
+        int rightSide = array[indexRight];
+        int leftSide = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            leftSide += array[i];
+        }
+        if (leftSide != rightSide) {
+            for (int i = indexRight; i >= 0; i--) {
+                if (indexRight == 0) {
+                    return false;
+                }
+                indexRight--;
+                rightSide += array[indexRight];
+                leftSide = 0;
+                for (int j = 0; j < indexRight; j++) {
+                    leftSide += array[j];
+                }
+                if (leftSide == rightSide) {
+                    return true;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static int[] shiftArray(int[] array, int shift) {
+        int indexLastEl = array.length - 1;
+        int cycle = Math.abs(shift);
+
+        while (cycle > 0) {
+            if (shift > 0) {
+                int lastEl = array[indexLastEl];
+                for (int i = indexLastEl - 1; i >= 0; i--) {
+                    array[i + 1] = array[i];
+                }
+                array[0] = lastEl;
+            }
+            if (shift < 0) {
+                int firstEl = array[0];
+                for (int i = 1; i <= indexLastEl; i++) {
+                    array[i - 1] = array[i];
+                }
+                array[indexLastEl] = firstEl;
+            }
+            cycle--;
+        }
+        return array;
+    }
+}
--- Lesson1/src/main/java/Lesson1/Lesson1.java
+++ Lesson1/src/main/java/Lesson1/Lesson1.java
@@ -1,7 +1,85 @@
 package Lesson1;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 public class Lesson1 {
+    // 1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // 2. Создать переменные всех пройденных типов данных, и инициализировать их значения;
+        byte myByte = 127;
+        short myShort = 32767;
+        int myInt = 2147483647;
+        long myLong = 922337245;
+        float myFloat = 8.5f;
+        double myDouble = 8.5;
+        boolean myBoolean = false;
+        char myChar = 'q';
+        BigDecimal myDecimal = new BigDecimal("123.030465805496804596845096");
+        String myString = "Hello";
+        //-----//
+
+        System.out.println(calc(2, 4, 0, 3));
+        System.out.println(numberComparison(5, 6));
+
+        numberCheck(7);
+        numberCheck(-3);
+        numberCheck(0);
+
+        System.out.println(isNegativeNumber(0));
+
+        greetings("Петр");
+
+        isLeapYear(2400);
+    }
+
+    //3. Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,где a, b, c, d – входные параметры этого метода;
+    private static int calc(int a, int b, int c, int d) {
+        return a * (b + (c / d));
+    }
+
+    //4. Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false;
+    private static boolean numberComparison(int a, int b) {
+        return ((a + b) >= 10 && (a + b) <= 20) ? true : false;
+    }
+
+    //5. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль положительное ли число передали, или отрицательное; Замечание: ноль считаем положительным числом.
+    private static void numberCheck(int number) {
+        if (number >= 0) {
+            System.out.println(number + " - положительное число");
+        } else {
+            System.out.println(number + " - отрицательное число");
+        }
+    }
+
+    //6. Написать метод, которому в качестве параметра передается целое число, метод должен вернуть true, если число отрицательное;
+    private static boolean isNegativeNumber(int number) {
+        return number < 0;
+    }
+
+    //7. Написать метод, которому в качестве параметра передается строка, обозначающая имя, метод должен вывести в консоль сообщение «Привет, указанное_имя!»;
+    private static void greetings(String name) {
+        if (name.length() > 0) {
+            System.out.println("Привет, " + name);
+        } else {
+            System.out.println("Имя не задано");
+        }
+    }
+
+    //8. * Написать метод, который определяет является ли год високосным, и выводит сообщение в консоль. Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+    private static void isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("Год високосный");
+                } else {
+                    System.out.println("Год не високосный");
+                }
+            } else {
+                System.out.println("Год високосный");
+            }
+        } else {
+            System.out.println("Год не високосный");
+        }
     }
 }
