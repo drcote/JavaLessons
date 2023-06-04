613438bbd6e9a4cdfd538c5c1d09cd860e0c5ed4
Denis Merezhin <merezhin@gmail.com>
2023-06-04 18:23:49 +0300

    refactoring кода

--- Lesson1/src/main/java/dz4/LimitedMethod.java
+++ Lesson1/src/main/java/dz4/LimitedMethod.java
@@ -6,28 +6,26 @@ public class LimitedMethod implements Method {
     private final Method method;
     private final int maxCallsPerMinute;
     private Queue<Date> queue;
-    private int callCount = 0;
-    private long lastCallTime = 0;
 
     long timeToQueue;
 
     public LimitedMethod(Method method, int maxCallsPerMinute, long timeToQueue) {
         this.method = method;
         this.maxCallsPerMinute = maxCallsPerMinute;
-        this.queue =  new LinkedList<>();
+        this.queue = new LinkedList<>();
         this.timeToQueue = timeToQueue;
 
     }
 
     @Override
     public void call() {
-        if(queue.size() >= maxCallsPerMinute){
+        if (queue.size() >= maxCallsPerMinute) {
             Date currentTime = new Date();
             if ((currentTime.getTime() - queue.peek().getTime()) > timeToQueue) {
                 queue.remove();
                 queue.offer(currentTime);
                 method.call();
-            } else{
+            } else {
                 System.out.println("Method is called too often, skipping call");
             }
         } else {
